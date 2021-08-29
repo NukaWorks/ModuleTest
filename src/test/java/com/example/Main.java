e63b1e9bcc8ce9e149b4be95c6330fd5a4c97322
Sundev79 <sundev79@nutdevs.org>
2021-08-30 01:53:15 +0200

    Added Main Test Class for testing.

--- src/test/java/com/example/Main.java
+++ src/test/java/com/example/Main.java
@@ -2,14 +2,18 @@ package com.example;
 
 import org.nutdevs.modularkit.core.ModularSource;
 import org.nutdevs.modularkit.core.ex.ModRegisterEx;
+import org.nutdevs.modularkit.core.ex.ModSourceEx;
 import org.nutdevs.modularkit.core.ex.ModUuidEx;
 
 import java.io.File;
 
+/**
+ * A simple test class for booting the TestModule external module installed on the given directory.
+ */
 public class Main {
 
-    public static void main(String[] args) throws ModUuidEx, ModRegisterEx {
-        ModularSource source = new ModularSource("98195aa7", new File("C:\\Users\\sunde\\Desktop\\NutDevs.org\\Projects\\ModuleTest\\Mods"));
+    public static void main(String[] args) throws ModUuidEx, ModRegisterEx, ModSourceEx {
+        ModularSource source = new ModularSource("98195aa7", new File("C:\\Users\\sunde\\Desktop\\NutDevs.org\\Projects\\ModuleTest\\Mods"), ".jar");
         // No need to register - external modules will be auto-registered
         source.getModuleManager().runModule(source.getModuleManager().findModuleByUuiD("758ad8e8")); // Find and run the module
     }
