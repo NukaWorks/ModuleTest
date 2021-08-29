a417047a3ba90cd1a57453c38e67ab55573f97ec
Sundev79 <sundev79@nutdevs.org>
2021-08-30 01:52:32 +0200

    Added the ModuleTest Mod
    It a simple module example.

--- src/main/java/com/example/ModuleTest.java
+++ src/main/java/com/example/ModuleTest.java
@@ -3,11 +3,25 @@ package com.example;
 import org.nutdevs.modularkit.core.ModularModule;
 import org.nutdevs.modularkit.core.ex.ModUuidEx;
 
+/**
+ * A simple ModularKit external module called "TestModule".
+ */
 public class ModuleTest extends ModularModule {
     public ModuleTest() throws ModUuidEx {
+        // simple constructor for create a valid ModularModule.
+
         super("ModuleTest", "758ad8e8", "Sundev79", "0.0.1");
+
+         /*
+            - Found a good name
+            - Go to https://uuidgenerator.net or run uuidgen on *nix systems and copy the first group of the uuid.
+            - And boom the prerequisites is complete !
+         */
     }
 
+    /**
+     * the runEvent(), called on the module is started (enabled).
+     */
     @Override
     public void runEvent() {
         System.out.println("Hello " + this.getModuleName() + " !");
