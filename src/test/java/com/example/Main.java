6dc67b20ad78ba13a50e95f2670717714727411e
Sundev79 <sundev79@nutdevs.org>
2022-01-08 15:25:14 +0100

    TestApp:File example directory

--- src/test/java/com/example/Main.java
+++ src/test/java/com/example/Main.java
@@ -13,7 +13,7 @@ import java.io.File;
 public class Main {
 
     public static void main(String[] args) throws ModUuidEx, ModRegisterEx, ModSourceEx {
-        ModularSource source = new ModularSource("98195aa7", new File("C:\\Users\\sunde\\Desktop\\NutDevs.org\\Projects\\ModuleTest\\Mods"), ".jar");
+        ModularSource source = new ModularSource("98195aa7", new File("/path/to/dir"), ".jar");
         // No need to register - external modules will be auto-registered
         source.getModuleManager().runModule(source.getModuleManager().findModuleByUuiD("758ad8e8")); // Find and run the module
     }
